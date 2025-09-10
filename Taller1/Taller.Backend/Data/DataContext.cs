c5901f2379dd03cac0df8c2c0e130020e16fc9f9
KEVIN DAVID LONDONO ROQUEME <kevinlondono1122890@correo.itm.edu.co>
2025-09-10 10:56:54 -0500

    Create database

---
+++ Taller1/Taller.Backend/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using Taller.Shared.Entities;
+
+namespace Taller.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
---
+++ Taller1/Taller.Shared/Entities/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taller.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre")]
+    [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Apellido")]
+    [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Activo")]
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Fecha de Contratación")]
+    [DataType(DataType.Date)]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public DateTime HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [DataType(DataType.Currency)]
+    [Range(0, double.MaxValue, ErrorMessage = "El campo {0} debe ser un número positivo.")]
+    [Precision(18, 2)] // 👈 define precisión y escala en la BD
+    public decimal Salary { get; set; }
+}
